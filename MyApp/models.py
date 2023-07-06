9557d6f9084799619bbc3ee43bd42e36082c440e
HemantJadhav <129278150+JadhavHemant@users.noreply.github.com>
2023-07-06 15:20:37 +0530

    Update models.py
    NA

--- MyApp/models.py
+++ MyApp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Employee(models.Model):
     employee_name =models.CharField(max_length=100)
+    employee_code=models.CharField(max_length=200)
+    designation=models.CharField(max_length=200)
+    salary=models.FloatField()

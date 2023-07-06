f1c8725411d29cc5103422ce28333eb34ceb4f6a
ShantanuBTECH <130627554+ShantanuBTECH@users.noreply.github.com>
2023-07-06 15:34:10 +0530

    Update models.py

--- MyApp/models.py
+++ MyApp/models.py
@@ -7,4 +7,10 @@ class Employee(models.Model):
     designation=models.CharField(max_length=200)
     salary=models.FloatField()
     class Meta:
-        db_table="employeestbl"
+        db_table="employeestbl" 
+        
+class StudentUserId(models.Model):
+    Student_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    class Meta:
+        db_table="tbl_user_student"      
